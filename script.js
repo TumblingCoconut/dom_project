f0ded5fa00c8454ec6a9677019fadb553310a7d1
Joseph Lam <josephl0406@icloud.com>
2024-07-29 11:37:42 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Add your code to this file
+document.body.style.backgroundColor = "rgb(0, 15, 133)";
+document.querySelector(".introduction h1").style.textDecoration = "underline";
+document.querySelector(".introduction h2").innerText = "Here are Pokemon's incredible Gen 1 starters!";
+
+document.querySelector("#starters_image_div img").style.border = "5px dotted orange"
